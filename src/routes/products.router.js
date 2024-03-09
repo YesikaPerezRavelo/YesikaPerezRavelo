6f7ab3c0373ee085c51fc22a18bec8e527d327b8
Yesika Perez Ravelo <yesikapr@gmail.com>
2024-03-09 20:09:43 -0300

    carga de imagenes y productos

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { uploader } from "../utils.js";
 
 const router = Router();
 
@@ -8,13 +9,21 @@ router.get("/", (req, res) => {
   res.send(products);
 });
 
-router.post("/", (req, res) => {
-  const { title, description, price, thumbnail, code, stock } = req.body;
+router.post("/", uploader.single("thumbnail"), (req, res) => {
+  if (!req.file) {
+    return res
+      .status(400)
+      .send({ error: "Se necesita cargar una imagen para crear producto!" });
+  }
 
-  if (!title || !description || !price || !thumbnail || !code || !stock)
+  const { title, description, price, code, stock } = req.body;
+
+  if (!title || !description || !price || !code || !stock)
     return res
       .status(400)
       .send({ error: "Faltan datos para crear el producto!" });
+
+  const thumbnail = req.file.path;
 
   products.push({ title, description, price, thumbnail, code, stock });
 
