76d6752d89e927fad7eea2651cf464b54069b2ab
Yesika Perez Ravelo <yesikapr@gmail.com>
2024-03-09 23:36:36 -0300

    primera entrega

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from "express";
 import productsRouter from "./routes/products.router.js";
 import cartRouter from "./routes/cart.router.js";
+import ProductManager from "./productManager.js";
 
 const app = express();
+const productManager = new ProductManager("../data/products.json");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -14,4 +16,33 @@ app.use(express.static("public"));
 const PORT = 8080;
 app.listen(PORT, () => {
   console.log(`Servidor activo http://localhost:${PORT}`);
+});
+
+// POST route using ProductManager method
+app.post("/api/products", async (req, res) => {
+  const productData = req.body;
+
+  if (!productManager.validateProductData(productData)) {
+    return res.status(400).send({ error: "Invalid product data." });
+  }
+
+  try {
+    await productManager.AddProduct(productData);
+    res.status(201).send({ message: "Producto creado correctamente." });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error: "Error interno del servidor." });
+  }
+});
+
+// PUT route using ProductManager method
+app.put("/api/products/:pid", async (req, res) => {
+  const pid = req.params.pid;
+  res.send(await productManager.UpdateProduct(pid, req.body));
+});
+
+// DELETE route using ProductManager method
+app.delete("/api/products/:pid", async (req, res) => {
+  const pid = req.params.pid;
+  res.send(await productManager.DeleteProduct(pid));
 });
---
+++ src/productManager.js
@@ -0,0 +1,143 @@
+import fs from "fs/promises";
+
+export default class ProductManager {
+  constructor(filePath) {
+    this.path = `./data/products.json`;
+    this.products = [];
+    this.loadProducts();
+    this.incrementId = this.calculateIncrementId();
+  }
+
+  async loadProducts() {
+    try {
+      const data = await fs.readFile(this.path, "utf-8");
+      return JSON.parse(data);
+    } catch (error) {
+      console.error(error);
+      this.product = [];
+    }
+  }
+
+  async saveProducts() {
+    try {
+      await fs.writeFile(
+        this.path,
+        JSON.stringify(this.products, null, 2),
+        "utf-8"
+      );
+    } catch (error) {
+      console.error(error);
+      console.error("Error guardando productos:", error);
+    }
+  }
+
+  calculateIncrementId() {
+    const maxId = this.products.reduce(
+      (max, product) => (product.id > max ? product.id : max),
+      0
+    );
+    return maxId + 1;
+  }
+
+  async addProduct(productData) {
+    console.log("Received product data:", productData);
+    if (
+      !productData.title ||
+      !productData.description ||
+      !productData.price ||
+      !productData.status ||
+      !productData.code ||
+      !productData.stock
+    ) {
+      console.error("Error: Todos los campos son obligatorios.");
+      return;
+    }
+
+    const codeExist = this.products.some(
+      (product) => product.code === productData.code
+    );
+
+    if (codeExist) {
+      console.error(
+        `Error: Producto con código ${productData.code} ya existe.`
+      );
+      return;
+    }
+
+    const product = {
+      id: this.incrementId++,
+      ...productData,
+      thumbnail: productData.thumbnail ?? [],
+    };
+
+    console.log(`Añadiendo producto...`);
+    this.products.push(product);
+    await this.saveProducts();
+    console.log(`${product.title} agregado.`);
+  }
+
+  getProducts(limit) {
+    console.log(`Buscando productos...`);
+    console.log(this.products);
+    if (limit) {
+      return this.products.slice(0, limit);
+    }
+    return this.products;
+  }
+
+  getProductById(id) {
+    const product = this.products.find((product) => product.id == id);
+
+    if (!product) {
+      console.error(`Error: Producto con id ${id} no encontrado.`);
+      return;
+    }
+    console.log(`Buscando producto con id ${id}...`);
+    console.log(product);
+    return product;
+  }
+
+  async updateProduct(id, updatedFields) {
+    if (!id || !updatedFields) {
+      console.error("Error: Todos los campos son obligatorios.");
+      return;
+    }
+
+    const index = this.products.findIndex((product) => product.id === id);
+
+    if (index === -1) {
+      console.error(`Error: Producto con ID ${id} no encontrado.`);
+      return;
+    }
+
+    if (
+      updatedFields.code &&
+      this.products.some((product) => product.code === updatedFields.code)
+    ) {
+      console.error(
+        `Error: Producto con código ${updatedFields.code} ya existe.`
+      );
+      return;
+    }
+
+    console.log(`Actualizando producto con ID ${id}`);
+
+    this.products[index] = { ...this.products[index], ...updatedFields };
+
+    await this.saveProducts();
+    console.log(`Producto con ID ${id} actualizado...`);
+    this.getProductById(id);
+  }
+
+  async deleteProduct(id) {
+    const index = this.products.findIndex((product) => product.id === id);
+    if (index === -1) {
+      console.error(`Error: Producto con id ${id} no encontrado.`);
+      return;
+    }
+    console.log(`Eliminando producto con id ${id}...`);
+    this.products = this.products.filter((product) => product.id !== id);
+    await this.saveProducts();
+    console.log(`Producto ${id} eliminado.`);
+  }
+}
